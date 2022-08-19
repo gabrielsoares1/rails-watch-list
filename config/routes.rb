5b3975225532eb8faeda9c3eb4f75ed329482471
Gabriel Teixeira Soares <gabriel_soares10@hotmail.com>
2022-08-19 16:29:23 -0300

    watch list

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :bookmarks do
+  end
+  resources :lists do
+    resources :bookmarks
+  end
+ 
 end
